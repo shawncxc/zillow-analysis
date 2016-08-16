a0f2574b5ab2c357836af5463b1b3d781a9a098a
xuchang chen <imxuchang@gmail.com>
2016-08-16 00:20:33 -0700

    get api for getting all streets

---
+++ config.js
@@ -0,0 +1,7 @@
+var zws_id = 'X1-ZWz1febdj6zu2z_axb7v';
+var streets_link = 'http://data.sfgov.org/resource/fuen-w6ja.json';
+
+module.exports = {
+  zws_id: zws_id,
+  streets_link: streets_link,
+};
--- get_data.js
+++ get_data.js
@@ -2,8 +2,23 @@ var fs = require('fs');
 var http = require('http');
 var parseString = require('xml2js').parseString;
 var json2csv = require('json2csv');
-var zws_id = 'X1-ZWz1febdj6zu2z_axb7v';
+var config = require('./config.js');
+var zws_id = config.zws_id;
+var streets_link = config.streets_link;
 
+// Get all streets
+http.get(streets_link, function(response) {
+  var data = '';
+  response.on('data', function(d) {
+    data += d.toString();
+  });
+  response.on('end', function() {
+    data = JSON.parse(data);
+    console.log(data);
+  });
+});
+
+// Hit zillow and convert to csv
 http.get({
   host: 'www.zillow.com',
   path: '/webservice/GetDeepSearchResults.htm?zws-id=' + zws_id + '&address=1188%20Mission%20St&citystatezip=San-Francisco%2C+CA'
@@ -15,6 +30,7 @@ http.get({
   response.on('end', function() {
     parseString(xml, function (err, res) {
       res = res['SearchResults:searchresults'].response[0].results[0].result;
+      console.log(res);
       var fields = ['zpid', 'links', 'address', 'finishedSqFt', 'bathrooms', 'bedrooms', 'zestimate', 'localRealEstate'];
       var csv = json2csv({ data: res, fields: fields });
        
