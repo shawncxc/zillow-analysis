ad7e097ed9b4af8da4bfa2b2b75e756a713410d0
xuchang chen <imxuchang@gmail.com>
2016-08-23 23:19:33 -0700

    loop address to get data and append csv

--- genCSV.js
+++ genCSV.js
@@ -2,55 +2,39 @@ var fs = require('fs');
 var parseString = require('xml2js').parseString;
 var json2csv = require('json2csv');
 var getData = require('./getData.js');
+var dataAdapter = require('./dataAdapter.js');
 var fields = [
-  'zpid', 'street', 'zipcode', 'city', 'state', 'latlon',
-  'bathrooms', 'bedrooms', 'rentzestimate', 'price',
+  'zpid', 
+  'latlon', 'street', 'zipcode', 'city', 'state',       // ADDRESS
+  'bedrooms', 'bathrooms', 'finishedSqFt','yearBuilt',  // PROPERTY
+  'price', 'rent', 'tax year', 'tax', 'hoa'             // MONEY
 ];
 
-var dataAdapter = function(data) {
-  data = data['SearchResults:searchresults'].response[0].results[0].result;
-  data = JSON.stringify(data);
-  data = data.replace(/[\[\]]+/g, '');
-  data = '[' + data + ']';
-  data = JSON.parse(data);
-
-  var res = [];
-  data.forEach(d => {
-    res.push({
-      zpid: d.zpid,
-      street: d.address.street,
-      zipcode: d.address.zipcode,
-      city: d.address.city,
-      state: d.address.state,
-      latlon: [d.address.latitude, d.address.longitude],
-      bathrooms: d.bathrooms,
-      bedrooms: d.bedrooms,
-      rentzestimate: d.rentzestimate ? d.rentzestimate.amount._ : 0,
-      price: d.localRealEstate.region.zindexValue,
-    });
-  });
-
-  return res;
-};
-
 function genCSV(address, city, state, genCSV) {
-  getData(address, city, state).then(function(data) {
-    parseString(data, function (err, res) {
-      if (err) {
-        console.error(err);
-      } else {
-        res = dataAdapter(res);
-        console.log(res);
-        if (genCSV) {
-          var csv = json2csv({ data: res, fields: fields }); 
-          fs.writeFile('result.csv', csv, function(err) {
-            if (err) { throw err; }
-            console.log('file saved');
-          });
+  setTimeout(function () {
+    getData(address, city, state).then(function(data) {
+      parseString(data, function (err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          res = dataAdapter(res);
+          if (!res) {
+            console.log('No Results.');
+            return; 
+          } else {
+            console.log('Results: ', res);
+          }
+          if (genCSV) {
+            var csv = json2csv({ data: res, fields: fields }); 
+            fs.writeFile('result.csv', csv, { flag: 'a' }, function(err) {
+              if (err) { throw err; }
+              console.log('file saved');
+            });
+          }
         }
-      }
+      });
     });
-  });
+  }, 1000);
 }
 
 module.exports = genCSV;
--- zillow-get.js
+++ zillow-get.js
@@ -11,9 +11,14 @@ command
   .option('-f, --flag', 'Generate CSV')
   .parse(process.argv);
 
+var args = command.args;
 if (command.address && command.city && command.state) {
-  var args = command.args;
   genCSV(args[0], args[1], args[2], args[3]);
+} else if (command.city && command.state) {
+  var addresses = ['1190 Mission St', '1188 Mission St', '500 Market St'];
+  addresses.forEach(addr => {
+    genCSV(addr, args[0], args[1], args[2]);  
+  });
 } else {
   console.log('You have to pass in all the fields that are required.');
 }
